81b4242b579bde93bb92855f338661b6293bb0c1
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-03-08 13:52:41 -0300

    fix: brand change

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,7 +1,23 @@
+import { Section } from '@/components/section'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Links | Patrick',
+  description: "I'm a full-stack software engineer.",
+}
+
 export default function Links() {
   return (
     <>
-      <div>Link</div>
+      <Section>
+        <div>Link</div>
+      </Section>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
     </>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,16 @@
 
 import { ToggleTheme } from './toggle-theme'
 import { Anchor } from './anchor'
-import brand from '../assets/brand-dark.svg'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { cn } from '@/lib/utils'
+import { useTheme } from 'next-themes'
 
 export function Header() {
   const [hasScrolled, setHasScrolled] = useState(false)
+  const { resolvedTheme } = useTheme()
+
+  const theme = resolvedTheme?.split(' ')[0]
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,7 +36,12 @@ export function Header() {
     >
       <nav className="px-4 md:px-6 py-3 max-w-[700px] mx-auto flex justify-between items-center gap-3">
         <div>
-          <Image alt="Brand" src={brand} />
+          <Image
+            width={36}
+            height={36}
+            alt="Brand"
+            src={`/brand/brand-${theme}.svg`}
+          />
         </div>
         <ul className="flex gap-6 text-sm text-ring font-medium">
           <Anchor link="projects" label="Projects" />
