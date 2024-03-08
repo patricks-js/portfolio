946e4b8e25478c8299f0b8ff795e9526cfd12fb6
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-03-08 12:14:49 -0300

    chore: add redirect to /links

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/links',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
